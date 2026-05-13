ce02b7ab76bd609bed0d3d730c7382f7d981e6f3
Mikhail Bolshakov <foxom@ya.ru>
2026-05-13 11:35:44 +0000

    Initial

---
+++ has_cycle.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        node = Node(value)
+
+        if self.head is None:
+            self.head = node
+            return
+
+        current = self.head
+        while current.next is not None:
+            current = current.next
+
+        current.next = node
+
+    def has_cycle(self):
+        slow = self.head
+        fast = self.head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next  # ошибка здесь
+
+            if slow == fast:
+                return True
+
+        return False
